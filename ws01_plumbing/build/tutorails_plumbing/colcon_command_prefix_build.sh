77d00fb32eaa20694030ec50a951bd43dad5b8d3
MaProjec <3344605101@qq.com>
2025-01-02 16:35:17 +0800

    元功能包

---
+++ ws01_plumbing/build/tutorails_plumbing/colcon_command_prefix_build.sh
@@ -0,0 +1,10 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/ros/ros_learn/ws01_plumbing/install/base_interfaces_demo/share/base_interfaces_demo/package.sh"
+. "/home/ros/ros_learn/ws01_plumbing/install/cpp04_param/share/cpp04_param/package.sh"
+. "/home/ros/ros_learn/ws01_plumbing/install/py04_param/share/py04_param/package.sh"
+. "/home/ros/ros_learn/ws01_plumbing/install/cpp01_topic/share/cpp01_topic/package.sh"
+. "/home/ros/ros_learn/ws01_plumbing/install/cpp02_service/share/cpp02_service/package.sh"
+. "/home/ros/ros_learn/ws01_plumbing/install/cpp03_action/share/cpp03_action/package.sh"
+. "/home/ros/ros_learn/ws01_plumbing/install/py01_topic/share/py01_topic/package.sh"
+. "/home/ros/ros_learn/ws01_plumbing/install/py02_service/share/py02_service/package.sh"
+. "/home/ros/ros_learn/ws01_plumbing/install/py03_action/share/py03_action/package.sh"
